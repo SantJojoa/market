c409bef692ae2830a9f1e49fc1e69b076325288d
Santiago Jojoa <santiago.jojoan@gmail.com>
2023-04-28 08:36:36 -0500

    V 1.2 cretaed login.html and updated php files

--- back/users/create_user.php
+++ back/users/create_user.php
@@ -1,14 +1,17 @@
 <?php
     include ("../config/cnx_db.php");
-    $first_name=$_POST['f_name'];
-    $last_name=$_POST['l_name'];
+    $first_name=$_POST['first_name'];
+    $last_name=$_POST['last_name'];
     $email=$_POST['email'];
-    $pass=$_POST['passwd'];
-    $sql="insert into users(f_name,l_name,email,passwd)
+    $pass=$_POST['password'];
+    $sql="insert into users(first_name,last_name,email,password)
     VALUES('$first_name','$last_name','$email','$pass')";
-    if($conn->query($sql)===TRUE){
-        echo"User has been created succesfully";
-    }else{
-        echo"Error: ".$conn->error;"<br>".$sql;
+    if($conn->query($sql)===TRUE){ //User has been created successfully
+        echo"<script>alert('User has been created successfully')</script>";
+        header("refresh:0;url=http://127.0.0.1/market/front/users/login.html");
+    }else{  //User dont be created
+        //echo"Error: ".$conn->error;"<br>".$sql;
+        echo"<script>alert('Email already exists')</script>";
+        header("refresh:0;url=htpp://127.0.0.1/market/back/users/create_user.php");
     }
 ?>
