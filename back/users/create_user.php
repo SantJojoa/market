3a65070dfbcd4a020648b4b0e4c430ee88dc2e0f
Santiago Jojoa <santiago.jojoan@gmail.com>
2023-06-05 18:26:07 -0500

    Update

--- back/users/create_user.php
+++ back/users/create_user.php
@@ -1,17 +1,16 @@
 <?php
-    include ("../config/cnx_db.php");
-    $first_name=$_POST['first_name'];
-    $last_name=$_POST['last_name'];
-    $email=$_POST['email'];
-    $pass=$_POST['passwords'];
-    $sql="insert into users(first_name,last_name,email,password)
-    VALUES('$first_name','$last_name','$email','$pass')";
-    if($conn->query($sql)===TRUE){ //User has been created successfully
+    include("../config/cnx_db.php");
+    $firstName = $_POST['f_name'];
+    $lastName = $_POST['l_name'];
+    $email = $_POST['email'];
+    $pass = $_POST['passwd'];
+
+    $sql = "INSERT INTO users (first_name,last_name,email,password) VALUES ('$firstName','$lastName','$email','$pass')" ;
+    if($conn->query($sql)===TRUE){
         echo"<script>alert('User has been created successfully')</script>";
-        header("refresh:0;url=http://127.0.0.1/market/front/users/login.html");
-    }else{  //User dont be created
-        //echo"Error: ".$conn->error;"<br>".$sql;
+        header("refresh:0; url=http://127.0.0.1/market/front/users/login.html");
+    }else{
         echo"<script>alert('Email already exists')</script>";
-        header("refresh:0;url=htpp://127.0.0.1/market/back/users/create_user.php");
+        header("refresh:0; url=http://127.0.0.1/market/front/users/create_user.html");
     }
 ?>
--- back/users/list_users.php
+++ back/users/list_users.php
@@ -1,38 +1,35 @@
-<?php include("../config/cnx_db.php"); ?>
+<?php  include("../config/cnx_db.php");?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List Users</title>
-    <link rel="stylesheet" href="../styles/styles.css">
+    <title>Document</title>
 </head>
-<body> 
-    <table align="center"  border="3">
-        <tr >
-            <th>First Name</th>
-            <th>Last Name</th>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <th>firstname</th>
+            <th>Lastname</th>
             <th>Email</th>
             <th>..</th>
         </tr>
-        <?php
-            $sql="SELECT * from users";
-            $result=$conn->query($sql);
-            if($result->num_rows > 0){
-                while($row=$result->fetch_assoc()){
-                    echo"<tr>
-                            <td>".$row['first_name']."</td>
-                            <td>".$row['last_name']."</td>
-                            <td>".$row['email']."</td>
-                            <td><a href='edit_user.php?id=".$row['id']."'><img src='../../front/icons/update.png.png'  width=30px></a> &nbsp;
-                                <a href='delate_user.php?id=".$row['id']."'><img src='../../front/icons/delate.png.png' width=30px></a></td>
+        <?php 
+            $sql= "SELECT * FROM users";
+            $result= $conn->query($sql);
+            if($result -> num_rows>0){
+                while ($row = $result->fetch_assoc()){
+                    echo "<tr>
+                            <td>".$row ['first_name']."</td>
+                            <td>".$row ['last_name']."</td>
+                            <td>".$row ['email']."</td>
+                            <td><a href='edit_user.php?id=".$row['id']."'><img src='../../front/icons/update.png.png' width='15'></a>&nbsp;
+                            <a href='delete_user.php?id=".$row['id']."'><img src='../../front/icons/delate.png.png' width='15'></a></td>
                         </tr>";
-
-                } 
-                
-            }
-        ?>
+                }
+            }?>
     </table>
 </body>
 </html>
